c58273afcb23a3f19f1b21649875e936156cf8d8
Marcus Williamson <hamawill@ucsc.edu>
2024-04-07 23:20:26 -0700

    Experiment 1 (generator)

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -23,12 +23,44 @@ class MyProjectClass {
 }
 
 function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
+  const fillers = {
+    action: ["Survive", "Experience Success With", "Not Die to", "Play", "Cook", "Discover", "Understand", "Approach", "Become", "Not Become", "Escape", "Deal With", "Fight", "Recognize", "Win", "Beat", "Piss Off", "Improve", "Spice Up"],
+    thing: ["Your Parents", "Chess", "Fish", "Cattle", "Farming", "School", "Your Life", "Public Speaking", "Meditation"],
+    descriptor: ["Ultimate", "Complete", "Essential", "Beginning", "Comprehensive", "Conclusive", "Life-Changing"],
+    audience: ["Gamers", "Dummies", "Idiots", "Students", "Nerds", "Programmers", "Artists", "Hitchhikers", "Athletes", "Amateurs", "Farmers", "Magicians", "Mystics", "Moms", "Psychologists", "Pastors", "Warriors", "Musicians", "Teens"],
+    topic: ["JavaScript", "Minecraft", "Carpentry", "Cattle", "Fishing", "the Galaxy", "Public Speaking", "Meditation", "Mushrooms", "Drugs"]
+  };
+  
+  const template = `How To $action $thing: The $descriptor Guide to $topic for $audience`;
+  
+  
+  // STUDENTS: You don't need to edit code below this line.
+  
+  const slotPattern = /\$(\w+)/;
+  
+  function replacer(match, name) {
+    let options = fillers[name];
+    if (options) {
+      return options[Math.floor(Math.random() * options.length)];
+    } else {
+      return `<UNKNOWN:${name}>`;
+    }
+  }
+  
+  function generate() {
+    let story = template;
+    while (story.match(slotPattern)) {
+      story = story.replace(slotPattern, replacer);
+    }
 
-  // call a method on the instance
-  myInstance.myMethod();
+    $("#box").text(story);
+  }
+
+  $("#clicker").click(generate);
+  
+  generate();
+  
 }
 
 // let's get this party started - uncomment me
-//main();
+main();
